9993cb20b0d9933545d53d36536526124f4c3508
Roman Arenas <roman.ea3232@gmail.com>
2024-01-31 16:46:29 -0300

    emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { Alerta } from "../utils";
-
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 
 
 export const Contact = () => {
+
+
+  const form = useRef();
 
   const [formData, setFormData] = useState({
     name: '',
@@ -22,9 +26,9 @@ export const Contact = () => {
     });
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
 
+  const sendEmail = (e) => {
+    e.preventDefault();
     if ([formData.name, formData.email, formData.message].includes("")) {
       setAlerta({ msg: 'Fields cannot be empty', error: true });
       setTimeout(() => {
@@ -37,11 +41,17 @@ export const Contact = () => {
       setTimeout(() => {
         setAlerta({})
       }, 3000);
+      emailjs.sendForm('service_fuseemb', 'template_idi6hl8', form.current, 'OXtnIgHGm_7Pcc9yu')
+        .then((result) => {
+          console.log(result.text);
+        }, (error) => {
+          console.log(error.text);
+        });
     } catch (error) {
       console.log(error)
     }
-  }
 
+  };
 
   const { msg } = alerta;
 
@@ -51,10 +61,10 @@ export const Contact = () => {
       <div id="form-container" className="bg-[--color1-l] p-10 rounded-lg shadow-2xl w-80 relative z-10 transform transition duration-500 ease-in-out lg:w-2/5">
         <h2 id="form-title" className="text-center text-3xl font-bold mb-10 text-[--color5-l] shadow-2xl p-3">Contact</h2>
         <form className="space-y-5">
-          <input type="text" className="w-full h-12 border border-[--color5-l]  px-3 rounded-lg" placeholder="name" id="name" value={formData.name} onChange={handleInputChange} />
-          <input className="w-full h-12 border border-[--color5-l]  px-3 rounded-lg" placeholder="Email" id="email" name="" type="email" value={formData.email} onChange={handleInputChange} />
-          <textarea className="w-full h-32 border border-[--color5-l] px-3 rounded-lg" placeholder="Message" id="message" name="" value={formData.message} onChange={handleInputChange}></textarea>
-          <button className="w-full h-12 bg-[--color4-l] hover:bg-[--color5-l] text-[--color1-l] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline uppercase lg:w-2/3" onClick={handleSubmit} >send message</button>
+          <input type="text" className="w-full h-12 border border-[--color5-l]  px-3 rounded-lg" name="name" placeholder="name" id="name" value={formData.name} onChange={handleInputChange} />
+          <input className="w-full h-12 border border-[--color5-l]  px-3 rounded-lg" name="email" placeholder="Email" id="email" type="email" value={formData.email} onChange={handleInputChange} />
+          <textarea className="w-full h-32 border border-[--color5-l] px-3 rounded-lg" placeholder="Message" id="message" name="message" value={formData.message} onChange={handleInputChange}></textarea>
+          <button className="w-full h-12 bg-[--color4-l] hover:bg-[--color5-l] text-[--color1-l] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline uppercase lg:w-2/3" onClick={sendEmail} >send message</button>
         </form>
         <div className="pt-2 w-full">
           {
